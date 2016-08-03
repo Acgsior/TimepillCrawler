bc8a79080c0d235826478c0e51abde7ed42dfda5
Morgan Qin <mqin@ML00060107.local>
2016-08-03 14:26:31 +0800

    - Date Time Format Bug: 0 - 11 AM/PM -> 1 - 12 AM/PM

--- src/main/java/com/acgsior/selector/impl/TimeObjectSelector.java
+++ src/main/java/com/acgsior/selector/impl/TimeObjectSelector.java
@@ -11,12 +11,12 @@ import java.util.Optional;
 /**
  * Created by mqin on 7/4/16.
  */
-public class TimeObjectSelector extends PropertySelector {
+public class TimeObjectSelector extends PropertySelector<LocalTime> {
 
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("K : m a", Locale.getDefault());
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("h : m a", Locale.getDefault());
 
     @Override
-    public LocalTime select(final Element element, final Optional parentId) {
+    public LocalTime select(final Element element, final Optional<String> parentId) {
         String dateText = element.select(getPattern()).get(getElementIndex()).text().toUpperCase();
         // all date with pattern  3 : 32 pm, so only reserve numbers to convert to date
         return LocalTime.parse(dateText, FORMATTER);
